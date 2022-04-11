49f9cdc5844d8d5eee5d893e8cfac1556bed8e9f
Phil Chang <phil.chang@yahooinc.com>
2022-04-11 18:16:03 +0800

    Initial Commit

---
+++ IRPlayer-swift/IRPlayer_swift.h
@@ -0,0 +1,19 @@
+//
+//  IRPlayer_swift.h
+//  IRPlayer-swift
+//
+//  Created by Phil Chang on 2022/4/11.
+//  Copyright © 2022 Yahoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRPlayer_swift.
+FOUNDATION_EXPORT double IRPlayer_swiftVersionNumber;
+
+//! Project version string for IRPlayer_swift.
+FOUNDATION_EXPORT const unsigned char IRPlayer_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRPlayer_swift/PublicHeader.h>
+
+
