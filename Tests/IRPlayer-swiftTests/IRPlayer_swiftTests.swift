fd78a20e46277b795bca8a8a780fbec5306d1436
Phil <irons7654321@gmail.com>
2026-05-24 17:48:46 +0800

    test: split FFPlayer test suite

--- Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
+++ Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
@@ -13,40 +13,6 @@ import simd
 import XCTest
 @testable import IRPlayer_swift
 
-final class IRFFPlayerTests: XCTestCase {
-
-    func testFactoryCreatesPlayerWhenAudioManagerIsMissing() {
-        let abstractPlayer = IRPlayerImp.player()
-        abstractPlayer.manager = nil
-
-        let ffPlayer = IRFFPlayer.player(with: abstractPlayer)
-
-        XCTAssertNil(ffPlayer.audioManager)
-        XCTAssertEqual(ffPlayer.duration, 0)
-        withExtendedLifetime(abstractPlayer) {}
-    }
-
-    func testPlayableBufferIntervalReloadsFFmpegDecoderBufferDuration() throws {
-        let player = IRPlayerImp.player()
-        player.decoder = IRPlayerDecoder.FFmpegDecoder()
-        player.manager = nil
-        player.replaceVideoWithURL(contentURL: NSURL(fileURLWithPath: "/tmp/missing.flv"))
-
-        let ffPlayer = try XCTUnwrap(mirroredFFPlayer(from: player))
-        addTeardownBlock {
-            ffPlayer.stop()
-        }
-        let decoder = try XCTUnwrap(ffPlayer.decoder)
-        XCTAssertEqual(decoder.minBufferedDuration, 2)
-
-        player.playableBufferInterval = 7
-
-        XCTAssertEqual(decoder.minBufferedDuration, 7)
-        withExtendedLifetime(player) {}
-    }
-
-}
-
 final class IRPlayerImpLazyPlayerTests: XCTestCase {
 
     func testLazyPlayerFactoriesReturnExistingPlayersOrCreateNewOnes() {
