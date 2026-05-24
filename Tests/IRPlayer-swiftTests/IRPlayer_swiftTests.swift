b1e53a67479eebd179602e95d084fb4ada84c44e
Phil <irons7654321@gmail.com>
2026-05-24 18:26:10 +0800

    test: split GL gesture controller suite

--- Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
+++ Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
@@ -13,45 +13,6 @@ import simd
 import XCTest
 @testable import IRPlayer_swift
 
-final class IRGLGestureControllerTests: XCTestCase {
-
-    func testClearingCurrentModeClearsSmoothScrollMode() {
-        let view = IRGLView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        let smoothScroll = IRSmoothScrollController(targetView: view)
-        let gestureController = IRGLGestureController()
-
-        gestureController.smoothScroll = smoothScroll
-        gestureController.currentMode = IRGLRenderMode2D()
-
-        gestureController.currentMode = nil
-
-        XCTAssertNil(smoothScroll.currentMode)
-        withExtendedLifetime(smoothScroll) {}
-    }
-
-    func testAddGestureReplacesExistingRotationGestureRecognizer() {
-        let view = IRGLView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        let gestureController = IRGLGestureController()
-
-        gestureController.addGesture(to: view)
-        gestureController.addGesture(to: view)
-
-        let rotationRecognizers = view.gestureRecognizers?.filter { $0 is UIRotationGestureRecognizer } ?? []
-        XCTAssertEqual(rotationRecognizers.count, 1)
-    }
-
-    func testRemoveGestureRemovesRotationGestureRecognizer() {
-        let view = IRGLView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        let gestureController = IRGLGestureController()
-
-        gestureController.addGesture(to: view)
-        gestureController.removeGesture(to: view)
-
-        let rotationRecognizers = view.gestureRecognizers?.filter { $0 is UIRotationGestureRecognizer } ?? []
-        XCTAssertTrue(rotationRecognizers.isEmpty)
-    }
-}
-
 final class IRGLViewSnapshotTests: XCTestCase {
 
     func testCreateImageFromFramebufferReturnsImageForZeroSizedView() {
