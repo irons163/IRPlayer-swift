0c969a50de4a8eb750cbdb1fedfe14506e5c763a
Phil <irons7654321@gmail.com>
2026-05-24 18:23:39 +0800

    test: split fisheye pano program suite

--- Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
+++ Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
@@ -13,21 +13,6 @@ import simd
 import XCTest
 @testable import IRPlayer_swift
 
-final class IRGLProgram2DFisheye2PanoTests: XCTestCase {
-
-    func testTextureSizeRejectsMissingParamsAndReturnsExistingDimensions() {
-        XCTAssertNil(IRGLProgram2DFisheye2Pano.textureSize(from: nil))
-
-        let params = IRGLFish2PanoShaderParams()
-        params.textureWidth = 1920
-        params.textureHeight = 960
-
-        let size = IRGLProgram2DFisheye2Pano.textureSize(from: params)
-        XCTAssertEqual(size?.width, 1920)
-        XCTAssertEqual(size?.height, 960)
-    }
-}
-
 final class IRGLGestureControllerTests: XCTestCase {
 
     func testClearingCurrentModeClearsSmoothScrollMode() {
