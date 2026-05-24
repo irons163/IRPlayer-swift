0f5c35e40edcc191b0a4d274f8a158c0551c9f6d
Phil <irons7654321@gmail.com>
2026-05-24 18:22:01 +0800

    test: split FF tools suite

--- Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
+++ Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
@@ -13,45 +13,6 @@ import simd
 import XCTest
 @testable import IRPlayer_swift
 
-final class IRFFToolsTests: XCTestCase {
-
-    func testFFLogIgnoresInvalidUTF8FormatString() throws {
-        let invalidFormat: [CChar] = [-1, 0]
-
-        try invalidFormat.withUnsafeBufferPointer { formatBuffer in
-            let format = try XCTUnwrap(formatBuffer.baseAddress)
-            withVaList([]) { args in
-                IRFFLog(context: nil, level: 0, format: format, args: args)
-            }
-        }
-    }
-
-    func testStreamTimebaseFallsBackToFiniteValueForInvalidStreamAndDefault() {
-        var stream = AVStream()
-        stream.time_base = AVRational(num: 0, den: 0)
-
-        let timebase = withUnsafePointer(to: &stream) { streamPointer in
-            IRFFStreamGetTimebase(streamPointer, defaultTimebase: 0)
-        }
-
-        XCTAssertEqual(timebase, 1)
-        XCTAssertTrue(timebase.isFinite)
-    }
-
-    func testStreamFPSFallsBackToFiniteValueForInvalidRatesAndTimebase() {
-        var stream = AVStream()
-        stream.avg_frame_rate = AVRational(num: 0, den: 0)
-        stream.r_frame_rate = AVRational(num: 0, den: 0)
-
-        let fps = withUnsafePointer(to: &stream) { streamPointer in
-            IRFFStreamGetFPS(streamPointer, timebase: 0)
-        }
-
-        XCTAssertEqual(fps, 1)
-        XCTAssertTrue(fps.isFinite)
-    }
-}
-
 final class IRGLProgram2DFisheye2PanoTests: XCTestCase {
 
     func testTextureSizeRejectsMissingParamsAndReturnsExistingDimensions() {
