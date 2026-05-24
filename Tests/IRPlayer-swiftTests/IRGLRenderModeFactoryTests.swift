a76aebfed415cc8142628aa97658d55dc6805b78
Phil <irons7654321@gmail.com>
2026-05-24 19:06:38 +0800

    test: cover render mode factory wiring

--- Tests/IRPlayer-swiftTests/IRGLRenderModeFactoryTests.swift
+++ Tests/IRPlayer-swiftTests/IRGLRenderModeFactoryTests.swift
@@ -21,17 +21,47 @@ final class IRGLRenderModeFactoryTests: XCTestCase {
     }
 
     func testFisheyeModesHaveExpectedOrderNamesAndDefaults() {
-        let modes = IRGLRenderModeFactory.createFisheyeModes(with: nil)
+        let parameter = IRMediaParameter(width: 100, height: 50)
+        let modes = IRGLRenderModeFactory.createFisheyeModes(with: parameter)
 
         XCTAssertEqual(modes.map(\.name), ["Panorama", "Onelen", "Fourlens", "Rawdata"])
         XCTAssertTrue(modes[0] is IRGLRenderMode2DFisheye2Pano)
         XCTAssertTrue(modes[1] is IRGLRenderMode3DFisheye)
         XCTAssertTrue(modes[2] is IRGLRenderModeMulti4P)
         XCTAssertTrue(modes[3] is IRGLRenderMode2D)
+        XCTAssertTrue(modes.allSatisfy { $0.parameter === parameter })
         XCTAssertEqual(modes[0].contentMode, .scaleAspectFill)
         XCTAssertEqual(modes[0].wideDegreeX, 360)
         XCTAssertEqual(modes[0].wideDegreeY, 20)
+        XCTAssertEqual(modes[0].shiftController.panAngle, 180)
+        XCTAssertEqual(modes[0].shiftController.tiltAngle, 360)
+        XCTAssertEqual(modes[1].shiftController.panAngle, 180)
+        XCTAssertEqual(modes[1].shiftController.tiltAngle, 360)
+        XCTAssertEqual(modes[2].shiftController.panAngle, 360)
+        XCTAssertEqual(modes[2].shiftController.tiltAngle, 360)
         XCTAssertFalse(modes[3].shiftController.enabled)
+    }
+
+    func testSpecializedFactoriesAttachParametersAndExpectedShiftAngles() {
+        let parameter = IRMediaParameter(width: 320, height: 240)
+
+        let vrMode = IRGLRenderModeFactory.createVRMode(with: parameter)
+        XCTAssertTrue(vrMode is IRGLRenderModeVR)
+        XCTAssertTrue(vrMode.parameter === parameter)
+        XCTAssertEqual(vrMode.shiftController.panAngle, 360)
+        XCTAssertEqual(vrMode.shiftController.tiltAngle, 180)
+
+        let distortionMode = IRGLRenderModeFactory.createDistortionMode(with: parameter)
+        XCTAssertTrue(distortionMode is IRGLRenderModeDistortion)
+        XCTAssertTrue(distortionMode.parameter === parameter)
+        XCTAssertEqual(distortionMode.shiftController.panAngle, 360)
+        XCTAssertEqual(distortionMode.shiftController.tiltAngle, 180)
+
+        let fisheyeMode = IRGLRenderModeFactory.createFisheyeMode(with: parameter)
+        XCTAssertTrue(fisheyeMode is IRGLRenderMode3DFisheye)
+        XCTAssertTrue(fisheyeMode.parameter === parameter)
+        XCTAssertEqual(fisheyeMode.shiftController.panAngle, 180)
+        XCTAssertEqual(fisheyeMode.shiftController.tiltAngle, 360)
     }
 
     func testPanoramaModeUsesScaleAspectFillAndWideDegrees() {
