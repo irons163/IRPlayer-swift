3a62e7cdad6d3823e4b9cb3da9fa2f73ab98d863
Phil <irons7654321@gmail.com>
2026-05-24 18:20:11 +0800

    test: split FF audio decoder suite

--- Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
+++ Tests/IRPlayer-swiftTests/IRPlayer_swiftTests.swift
@@ -13,50 +13,6 @@ import simd
 import XCTest
 @testable import IRPlayer_swift
 
-final class IRFFAudioDecoderTests: XCTestCase {
-
-    func testAudioDataBufferRejectsMissingDecodedData() {
-        XCTAssertNil(IRFFAudioDecoder.audioDataBuffer(fromDecodedData: nil))
-
-        var byte: UInt8 = 0
-        withUnsafeMutablePointer(to: &byte) { pointer in
-            let rawPointer = UnsafeMutableRawPointer(pointer)
-            XCTAssertEqual(IRFFAudioDecoder.audioDataBuffer(fromDecodedData: pointer), rawPointer)
-        }
-    }
-
-    func testAudioDataBufferRejectsMissingSwrBuffer() {
-        XCTAssertNil(IRFFAudioDecoder.audioDataBuffer(fromSwrBuffer: nil))
-
-        var byte: UInt8 = 0
-        withUnsafeMutablePointer(to: &byte) { pointer in
-            let rawPointer = UnsafeMutableRawPointer(pointer)
-            XCTAssertEqual(IRFFAudioDecoder.audioDataBuffer(fromSwrBuffer: rawPointer), rawPointer)
-        }
-    }
-
-    func testInputChannelCapacityRejectsMissingOrInvalidCodecContext() {
-        XCTAssertNil(IRFFAudioDecoder.inputChannelCapacity(from: nil))
-
-        var codecContext = AVCodecContext()
-        codecContext.channels = 0
-
-        withUnsafeMutablePointer(to: &codecContext) { contextPointer in
-            XCTAssertNil(IRFFAudioDecoder.inputChannelCapacity(from: contextPointer))
-
-            contextPointer.pointee.channels = 2
-            XCTAssertEqual(IRFFAudioDecoder.inputChannelCapacity(from: contextPointer), 2)
-        }
-    }
-
-    func testSampleElementCountRejectsEmptyInputs() {
-        XCTAssertNil(IRFFAudioDecoder.sampleElementCount(numberOfFrames: 0, channelCount: 2))
-        XCTAssertNil(IRFFAudioDecoder.sampleElementCount(numberOfFrames: 4, channelCount: 0))
-        XCTAssertNil(IRFFAudioDecoder.sampleElementCount(numberOfFrames: -1, channelCount: 2))
-        XCTAssertEqual(IRFFAudioDecoder.sampleElementCount(numberOfFrames: 3, channelCount: 2), 6)
-    }
-}
-
 final class IRFFToolsTests: XCTestCase {
 
     func testFFLogIgnoresInvalidUTF8FormatString() throws {
