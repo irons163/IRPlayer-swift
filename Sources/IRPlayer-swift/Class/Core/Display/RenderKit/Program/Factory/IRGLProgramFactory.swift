bc825aa40b6a39a45686dc4ec13b5052a6f58e80
Phil <irons7654321@gmail.com>
2026-05-31 11:52:05 +0800

    feat: add program factory policy helper

--- Sources/IRPlayer-swift/Class/Core/Display/RenderKit/Program/Factory/IRGLProgramFactory.swift
+++ Sources/IRPlayer-swift/Class/Core/Display/RenderKit/Program/Factory/IRGLProgramFactory.swift
@@ -7,6 +7,52 @@
 
 import Foundation
 
+enum IRGLProgramFactoryPolicy {
+    static func expandedScaleRange(from range: IRGLScaleRange, multiplier: Float) -> IRGLScaleRange {
+        IRGLScaleRange(minScaleX: range.minScaleX,
+                       minScaleY: range.minScaleY,
+                       maxScaleX: range.maxScaleX * multiplier,
+                       maxScaleY: range.maxScaleY * multiplier,
+                       defaultScaleX: range.defaultScaleX,
+                       defaultScaleY: range.defaultScaleY)
+    }
+
+    static func fisheyeScopeRange(from range: IRGLScopeRange, latmax: Float) -> IRGLScopeRange {
+        let maxLat = range.maxLat > 0 ? latmax : latmax - 90.0
+        var defaultLat = range.defaultLat
+        if defaultLat > maxLat || defaultLat < range.minLat {
+            defaultLat = (maxLat + range.minLat) / 2
+        }
+        return IRGLScopeRange(minLat: range.minLat,
+                              maxLat: maxLat,
+                              minLng: range.minLng,
+                              maxLng: range.maxLng,
+                              defaultLat: defaultLat,
+                              defaultLng: range.defaultLng)
+    }
+
+    static func defaultFisheyeScope(from range: IRGLScopeRange, panelIndex: Int?) -> IRGLScopeRange {
+        let defaultLng: Float
+        switch panelIndex {
+        case 1:
+            defaultLng = 180
+        case 2:
+            defaultLng = 270
+        case 3:
+            defaultLng = 0
+        default:
+            defaultLng = 90
+        }
+
+        return IRGLScopeRange(minLat: range.minLat,
+                              maxLat: range.maxLat,
+                              minLng: range.minLng,
+                              maxLng: range.maxLng,
+                              defaultLat: -40,
+                              defaultLng: defaultLng)
+    }
+}
+
 @objcMembers public class IRGLProgramFactory: NSObject {
 
     public static func createIRGLProgram2D(pixelFormat: IRPixelFormat, viewportRange: CGRect, parameter: IRMediaParameter?) -> IRGLProgram2D {
