820875b467256cdb3aa8d331627ac90e361e08b9
Phil <irons7654321@gmail.com>
2026-06-02 01:09:17 +0800

    test: split decoder packet policy tests

--- Tests/IRPlayer-swiftTests/IRFFDecoderOperationTests.swift
+++ Tests/IRPlayer-swiftTests/IRFFDecoderOperationTests.swift
@@ -68,62 +68,6 @@ final class IRFFDecoderOperationTests: XCTestCase {
         )
     }
 
-    func testPacketBufferBackpressureSleepIntervalUsesConfiguredThresholdAndPauseState() {
-        XCTAssertNil(
-            IRFFDecoder.packetBufferBackpressureSleepInterval(
-                audioSize: 3,
-                videoPacketSize: 6,
-                maxBufferSize: 10,
-                paused: false
-            )
-        )
-        XCTAssertEqual(
-            IRFFDecoder.packetBufferBackpressureSleepInterval(
-                audioSize: 4,
-                videoPacketSize: 6,
-                maxBufferSize: 10,
-                paused: false
-            ),
-            0.1
-        )
-        XCTAssertEqual(
-            IRFFDecoder.packetBufferBackpressureSleepInterval(
-                audioSize: 5,
-                videoPacketSize: 6,
-                maxBufferSize: 10,
-                paused: true
-            ),
-            0.5
-        )
-    }
-
-    func testPacketBufferBackpressureSleepIntervalRejectsInvalidSizes() {
-        XCTAssertNil(
-            IRFFDecoder.packetBufferBackpressureSleepInterval(
-                audioSize: -1,
-                videoPacketSize: 6,
-                maxBufferSize: 10,
-                paused: false
-            )
-        )
-        XCTAssertNil(
-            IRFFDecoder.packetBufferBackpressureSleepInterval(
-                audioSize: 4,
-                videoPacketSize: -1,
-                maxBufferSize: 10,
-                paused: false
-            )
-        )
-        XCTAssertNil(
-            IRFFDecoder.packetBufferBackpressureSleepInterval(
-                audioSize: 4,
-                videoPacketSize: 6,
-                maxBufferSize: 0,
-                paused: false
-            )
-        )
-    }
-
     func testShouldFetchAudioFrameRequiresActiveAudioPlaybackState() {
         XCTAssertTrue(
             IRFFDecoder.shouldFetchAudioFrame(
@@ -194,48 +138,6 @@ final class IRFFDecoderOperationTests: XCTestCase {
                 playbackFinished: false,
                 audioEnabled: false
             )
-        )
-    }
-
-    func testReadPacketEOFTransitionOnlyAppliesForNegativeReadResults() {
-        XCTAssertNil(IRFFDecoder.readPacketEOFTransition(readFrameResult: 0))
-        XCTAssertNil(IRFFDecoder.readPacketEOFTransition(readFrameResult: 1))
-        XCTAssertEqual(
-            IRFFDecoder.readPacketEOFTransition(readFrameResult: nil),
-            IRFFDecoder.ReadPacketEOFTransition(
-                endOfFile: true,
-                videoEndOfFile: true,
-                shouldFinishReadLoop: true,
-                shouldNotifyDelegate: true
-            )
-        )
-        XCTAssertEqual(
-            IRFFDecoder.readPacketEOFTransition(readFrameResult: -1),
-            IRFFDecoder.ReadPacketEOFTransition(
-                endOfFile: true,
-                videoEndOfFile: true,
-                shouldFinishReadLoop: true,
-                shouldNotifyDelegate: true
-            )
-        )
-    }
-
-    func testPacketRouteMatchesVideoAndAudioTrackIndexes() {
-        XCTAssertEqual(
-            IRFFDecoder.packetRoute(streamIndex: 2, videoTrackIndex: 2, audioTrackIndex: 3),
-            .video
-        )
-        XCTAssertEqual(
-            IRFFDecoder.packetRoute(streamIndex: 3, videoTrackIndex: 2, audioTrackIndex: 3),
-            .audio
-        )
-        XCTAssertEqual(
-            IRFFDecoder.packetRoute(streamIndex: 4, videoTrackIndex: 2, audioTrackIndex: 3),
-            .ignored
-        )
-        XCTAssertEqual(
-            IRFFDecoder.packetRoute(streamIndex: 0, videoTrackIndex: nil, audioTrackIndex: nil),
-            .ignored
         )
     }
 
